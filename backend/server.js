de5273d1cb756c2a3a024b9b1a0042de72494e6f
Patrick Nguyen <76218662+Pattypooper@users.noreply.github.com>
2024-10-04 18:05:55 -0700

    Cleaning up

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
+app.use(express.json()); 
 
 app.use(cors({
   origin: ['http://localhost:3000', 'https://accounts.spotify.com'], 
@@ -18,12 +19,20 @@ const topArtistsRoutes = require('./Routes/topArtists');
 const topTrackRoutes = require('./Routes/topTracks');
 const topGenreRoutes = require('./Routes/topGenres');
 const recentlyPlayedRoutes = require ('./Routes/recentlyPlayed');
+const getUserProfileRoutes = require('./Routes/getUserProfile')
+const createPlaylistRoute = require('./Routes/createPlaylists');
+const addTracksRoute = require('./Routes/addItems')
+const getRecommendationsRoute = require('./Routes/getRecommendations')
 
 app.use(authRoutes);
 app.use(topArtistsRoutes);
 app.use(topTrackRoutes);
 app.use(topGenreRoutes);
 app.use(recentlyPlayedRoutes);
+app.use(getUserProfileRoutes);
+app.use(createPlaylistRoute);
+app.use(addTracksRoute)
+app.use(getRecommendationsRoute)
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
