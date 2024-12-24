cc782ff54813827c232715c04daa6fef81bd2ef7
patrickhcnguyen <patricknguyen714@gmail.com>
2024-12-24 02:16:17 -0800

    New landing page

---
+++ frontend/src/Components/Landing Page/landingPage.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Navbar from '../../Components/Navbar/navbar';
+import backgroundImage from '../../assets/background/background.svg';
+
+const LandingPage: React.FC = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar title={''} isLoggedIn={false} onLogin={function (): void {
+        throw new Error('Function not implemented.');
+      }} onLogout={function (): void {
+        throw new Error('Function not implemented.');
+      }} />
+      <div 
+        className="h-screen bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: `url(${backgroundImage})` }}
+      >
+        <div className="container mx-auto px-4 h-full flex flex-col justify-start">
+          <div className="text-center pt-[20vh]">
+            <h1 className="text-[150px] leading-none font-bold mb-10 font-picnic text-white 
+                         lg:text-[120px] md:text-[90px] sm:text-[60px] xs:text-[40px]">
+              Welcome to Statify
+            </h1>
+            <div className="max-w-[968px] mx-auto">
+              <p className="text-[50px] font-picnic text-white leading-tight
+                          lg:text-[40px] md:text-[30px] sm:text-[30px] xs:text-[25px]">
+                View your most listened tracks, artists, and genres and switch between 3 different time periods. Your data updates every day!
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
