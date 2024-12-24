64d22730d3ed208c89360a854285b812c2e2a0aa
patrickhcnguyen <patricknguyen714@gmail.com>
2024-12-24 01:57:23 -0800

    updated tailwind stuff

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,7 +3,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'picnic': ['PicNic', 'sans-serif'],
+        'pixelify': ['PixelifySans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
