fa1420a56643bce0f4fefd759e3a2d2467701135
patrickhcnguyen <patricknguyen714@gmail.com>
2025-01-07 10:22:31 -0800

    User following scp[e
    g

--- backend/Routes/auth.js
+++ backend/Routes/auth.js
@@ -26,7 +26,7 @@ router.get("/", (req, res) => {
 
 router.get('/login', function(req, res) {
     const state = generateRandomString(16);
-    const scope = 'user-read-private user-read-email user-read-recently-played user-top-read playlist-modify-public playlist-modify-private ugc-image-upload';
+    const scope = 'user-read-private user-read-email user-read-recently-played user-top-read playlist-modify-public playlist-modify-private ugc-image-upload user-follow-read';
 
     res.redirect('https://accounts.spotify.com/authorize?' +
       querystring.stringify({
