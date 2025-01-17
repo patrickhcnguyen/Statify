cf6be7412261a75c8dc9e7c46e8b98acf9231e3b
patrickhcnguyen <patricknguyen714@gmail.com>
2025-01-17 11:25:22 -0800

    cleaning up stuff

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,11 @@ module.exports = {
       fontFamily: {
         'picnic': ['PicNic', 'sans-serif'],
         'pixelify': ['PixelifySans', 'sans-serif']
+      },
+      fontSize: {
+        'responsive-sm': 'clamp(0.75rem, 1.5vw, 1rem)',
+        'responsive-base': 'clamp(1rem, 2vw, 1.25rem)',
+        'responsive-lg': 'clamp(1.25rem, 2.5vw, 1.5rem)',
       }
     },
   },
