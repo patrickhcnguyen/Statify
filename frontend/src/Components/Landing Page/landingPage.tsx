a5c765ea373dced122b653de6d23f4b4739f0e45
patrickhcnguyen <patricknguyen714@gmail.com>
2025-01-30 18:54:52 -0800

    mobile UI changes

--- frontend/src/Components/Landing Page/landingPage.tsx
+++ frontend/src/Components/Landing Page/landingPage.tsx
@@ -5,7 +5,6 @@ import backgroundImage from '../../assets/background/background.svg';
 const LandingPage: React.FC = () => {
   return (
     <div 
-    // fix bg image 
       className="min-h-screen bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
@@ -14,14 +13,13 @@ const LandingPage: React.FC = () => {
       }} onLogout={function (): void {
       }} />
       <div className="container mx-auto px-4 h-full flex flex-col justify-start">
-        <div className="text-center pt-40">
-          {/* fix welcome message  */}
+        <div className="text-center pt-20 md:pt-40">
           <h1 className="text-[80px] leading-none font-bold mb-10 font-picnic text-white 
                         md:text-[90px] lg:text-[150px]">
             Welcome to Statify
           </h1>
           <div className="max-w-[968px] mx-auto">
-            <p className="text-[50px] font-pixelify text-white leading-tight
+            <p className="text-[45px] font-pixelify text-white leading-tight
                           lg:text-[40px] md:text-[30px]">
               View your most listened tracks, artists, and genres and switch between 3 different time periods. Your data updates every day!
             </p>
