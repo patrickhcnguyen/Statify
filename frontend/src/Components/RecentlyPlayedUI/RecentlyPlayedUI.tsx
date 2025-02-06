683c37e1aa3160428ae7adca753bbe25397910e1
patrickhcnguyen <patricknguyen714@gmail.com>
2025-02-05 17:30:15 -0800

    updated mobile UI

--- frontend/src/Components/RecentlyPlayedUI/RecentlyPlayedUI.tsx
+++ frontend/src/Components/RecentlyPlayedUI/RecentlyPlayedUI.tsx
@@ -70,7 +70,6 @@ const RecentlyPlayedUI: React.FC<RecentlyPlayedUIProps> = ({
 
   const renderShelf = (startIndex: number) => {
     const shelfTracks = tracks.slice(startIndex, startIndex + 4);
-    
     return (
       <div className="relative ml-[5%]">
         <div className="relative">
@@ -105,7 +104,9 @@ const RecentlyPlayedUI: React.FC<RecentlyPlayedUIProps> = ({
                   </p>
                 </div>
 
-                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 transition-opacity duration-200 bg-black/80 p-2 rounded-lg whitespace-nowrap z-30">
+                <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                              opacity-0 group-hover:opacity-100 transition-opacity duration-200 
+                              bg-black/80 p-2 rounded-lg whitespace-nowrap z-30">
                   <p className="font-pixelify text-white text-[0.9vw] min-text-[12px] max-text-[14px]">
                     {track.name}
                   </p>
@@ -126,34 +127,126 @@ const RecentlyPlayedUI: React.FC<RecentlyPlayedUIProps> = ({
     );
   };
 
+  const renderMobileShelf = (startIndex: number) => {
+    const shelfTracks = tracks.slice(startIndex, startIndex + 2);
+    return (
+      <div className="relative">
+        <div className="absolute bottom-[19px] left-1/2 -translate-x-1/2 flex gap-[25vw] z-10 w-full justify-center">
+          {shelfTracks.map((track, index) => (
+            <div 
+              key={startIndex + index} 
+              className={`relative cursor-pointer transition-all duration-200 group ${
+                selectedTracks.has(track.uri) 
+                  ? 'scale-105' 
+                  : 'hover:scale-105'
+              }`}
+              onClick={() => handleTrackClick(track)}
+            >
+              <div className="absolute -top-8 left-1/2 -translate-x-1/2 w-[30vw] z-20">
+                <p className="font-pixelify text-white text-center text-[14px] truncate">
+                  {track.name}
+                </p>
+              </div>
+
+              <img 
+                src={track.albumImageUrl}
+                alt={track.name}
+                className={`w-[25vw] h-[25vw] min-w-[90px] min-h-[90px] object-cover z-10 ${
+                  selectedTracks.has(track.uri) ? 'ring-2 ring-white' : ''
+                }`}
+              />
+
+              <div className="absolute bottom-[-23px] left-1/2 -translate-x-1/2 w-[30vw] z-20">
+                <p className="font-pixelify text-white text-center text-[12px] truncate">
+                  {track.artist}
+                </p>
+              </div>
+
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 
+                            opacity-0 group-hover:opacity-100 transition-opacity duration-200 
+                            bg-black/80 p-2 rounded-lg whitespace-nowrap z-30">
+                <p className="font-pixelify text-white text-[12px]">
+                  {track.name}
+                </p>
+                <p className="font-pixelify text-white/70 text-[10px] mt-1">
+                  {track.artist}
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <img 
+          src={shelfImage} 
+          alt="shelf" 
+          className="w-[80%] min-w-[300px] max-w-[720px] h-auto relative z-0 mx-auto"
+        />
+      </div>
+    );
+  };
+
   return (
     <div className="min-h-screen flex flex-col">
-      <div 
-        className="flex-1 bg-cover bg-center bg-no-repeat min-h-screen pb-[20vh]"
-        style={{ backgroundImage: `url(${backgroundImage})` }}
-      >
-        <div className="absolute top-[35vh] left-[70%] -translate-x-1/2 flex flex-col items-center">
+      {/* Mobile section */}
+      <div className="md:hidden">
+        {/* shelves */}
+        <h1 className="text-center mt-10 text-white text-[48px] font-pixelify">
+          Recently Played
+        </h1>
+        <div className="flex flex-col space-y-48 mt-[20rem] mb-[10rem]">
+          {[0, 2, 4, 6, 8, 10, 12, 14].map((startIndex) => (
+            <div key={startIndex}>
+              {renderMobileShelf(startIndex)}
+            </div>
+          ))}
+        </div>
+        {/* create playlist */}
+        <div className="flex justify-center mt-[-6rem] mb-[2rem]">
           <CreatePlaylist 
             userId={userProfile.id} 
             displayName={userProfile.displayName} 
-            topTracks={recentTracks} 
-            timeQuery={null} 
+            topTracks={recentTracks}
+            timeQuery={null}
           />
-          <div className="mt-[5vw]">
-            <Recommender 
-              topTracks={recentTracks} 
-              selectedTracks={Array.from(selectedTracks)}
-              displayedTracks={tracks}
-            />
-          </div>
         </div>
-        <div className="flex flex-col">
-          <div className="mt-[25vh] space-y-[20vh]">
-            {[0, 4, 8, 12].map((startIndex) => (
-              <div key={startIndex}>
-                {renderShelf(startIndex)}
-              </div>
-            ))}
+        {/* recommender */}
+        <div className="flex justify-center mb-8">
+          <Recommender 
+            topTracks={recentTracks}
+            selectedTracks={Array.from(selectedTracks)}
+            displayedTracks={tracks}
+          />
+        </div>
+      </div>
+
+      {/* Desktop section */}
+      <div className="hidden md:block">
+        <div 
+          className="flex-1 bg-cover bg-center bg-no-repeat min-h-screen pb-[20vh]"
+          style={{ backgroundImage: `url(${backgroundImage})` }}
+        >
+          <div className="absolute top-[35vh] left-[70%] -translate-x-1/2 flex flex-col items-center">
+            <CreatePlaylist 
+              userId={userProfile.id} 
+              displayName={userProfile.displayName} 
+              topTracks={recentTracks}
+              timeQuery={null}
+            />
+            <div className="mt-[5vw]">
+              <Recommender 
+                topTracks={recentTracks}
+                selectedTracks={Array.from(selectedTracks)}
+                displayedTracks={tracks}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col">
+            <div className="mt-[25vh] space-y-[20vh]">
+              {[0, 4, 8, 12].map((startIndex) => (
+                <div key={startIndex}>
+                  {renderShelf(startIndex)}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
