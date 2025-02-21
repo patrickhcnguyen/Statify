ac63635ec9966c54c8db2225d69c23a760873570
patrickhcnguyen <patricknguyen714@gmail.com>
2025-02-21 15:54:47 -0800

    responsive top artists page now

--- frontend/src/Components/TopArtistsUI/TopArtistsUI.tsx
+++ frontend/src/Components/TopArtistsUI/TopArtistsUI.tsx
@@ -323,7 +323,6 @@ const TopArtistsUI: React.FC<TopArtistsUIProps> = ({ topArtists, timeRange, setT
       {/* desktop view */}
       <div className="hidden md:block">
         <div className="relative flex items-center justify-center min-h-screen">
-          {/* Journal container - adjusted to fit journal image exactly */}
           <div className="relative">
             <img 
               src={journalImage} 
@@ -335,147 +334,13 @@ const TopArtistsUI: React.FC<TopArtistsUIProps> = ({ topArtists, timeRange, setT
             <div className="absolute inset-0">
               {currentArtist && (
                 <>
-                  {/* Main artist image - top left */}
-                  <div className="absolute left-0 top-16 border-2 border-red-500">
-                    <img
-                      src={currentArtist.albumImageUrl}
-                      alt={currentArtist.name}
-                      className="w-[175px] aspect-square"
-                    />
-                  </div>
-
-                  {/* Random album image */}
-                  <div className="absolute left-1/3 top-[45%]">
-                    <img
-                      src={currentArtist.randomAlbumImage}
-                      alt="Random Album"
-                      className="w-[175px] aspect-square"
-                    />
-                  </div>
-
-                  {/* Artist info section */}
-                  <div className="absolute left-1/3 top-[15%]">
-                    <span className="font-pixelify text-[48px] text-black">#{currentArtistIndex + 1}</span>
-                    <span className="font-pixelify text-[20px] text-black ml-4 inline-block translate-y-4">
-                      {currentArtist.name}
-                    </span>
-                    {/* Genre text */}
-                    <div className="font-pixelify text-[20px] text-black mt-[15%] -ml-8 max-w-[186px]">
-                      {displayText}
-                      {hasMore && (
-                        <button
-                          onClick={() => setIsExpanded(!isExpanded)}
-                          className="text-green-700 hover:text-green-900 ml-2 underline cursor-pointer"
-                        >
-                          {isExpanded ? 'Show Less' : 'Show More'}
-                        </button>
-                      )}
-                    </div>
-                  </div>
-
-                  {/* Followers count */}
-                  <div className="absolute left-[21%] top-[43%] w-[14%] min-h-[112px] font-pixelify text-[20px] text-black">
-                    {`They have ${currentArtist.followers.toLocaleString()} followers${currentArtist.isFollowed ? " and you're one of them!" : '!'}`}
-                  </div>
-
-                  {/* Decorative elements */}
-                  <img
-                    src={starImage}
-                    alt="Star 1"
-                    className="absolute w-[75px] aspect-square left-[40%] top-[35%]"
-                  />
-                  <img
-                    src={starImage}
-                    alt="Star 2"
-                    className="absolute w-[75px] aspect-square left-[30%] top-[50%]"
-                  />
-                  <img
-                    src={buttonImage}
-                    alt="Button"
-                    className="absolute w-[50px] aspect-square left-[32%] top-[55%]"
-                  />
-                  <img
-                    src={heartImage}
-                    alt="Heart"
-                    className="absolute w-[50px] aspect-square left-[64%] top-[35%]"
-                  />
-
-                  {/* Jellyfish section with top tracks */}
-                  <div className="absolute left-[51%] top-[14%] w-[21%] h-[39%]">
-                    <img
-                      src={jellyfishImage}
-                      alt="Jellyfish"
-                      className="w-full h-full max-w-[250px]"
-                    />
-                    <div className="absolute left-[8%] top-[10%] font-pixelify text-[20px] text-black text-center w-[40%]">
-                      Their top 5 songs are
-                    </div>
-                    <div className="absolute left-[55%] top-[10%] flex flex-col gap-2 w-[180px]">
-                      {currentArtist.topTracks?.slice(0, 5).map((track, index) => (
-                        <div key={index} className="flex items-center gap-2">
-                          <span className="text-[20px] font-bold">{index + 1}.</span>
-                          <a 
-                            href={`https://open.spotify.com/track/${track.uri.split(':')[2]}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-[20px] hover:text-green-700 transition-colors duration-200 truncate"
-                          >
-                            {track.name}
-                          </a>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-
-                  {/* Ribbon and recommendations section */}
-                  <div className="absolute left-[55%] top-[50%]">
-                    <img
-                      src={ribbonImage}
-                      alt="Ribbon"
-                      className="w-[200px] h-[140px]"
-                    />
-                    <div className="absolute left-[15%] top-[40%] flex flex-col gap-3 w-[144px]">
-                      {loading ? (
-                        <div>Loading recommendations...</div>
-                      ) : recommendations.length > 0 ? (
-                        recommendations.map((track, index) => (
-                          <a 
-                            key={track.uri}
-                            href={`https://open.spotify.com/track/${track.uri.split(':')[2]}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-left hover:text-green-700 transition-colors duration-200 truncate"
-                          >
-                            {index + 1}. {track.name}
-                          </a>
-                        ))
-                      ) : (
-                        <div>No recommendations found</div>
-                      )}
-                    </div>
-                  </div>
-
-                  {/* Cardboard Cat section */}
-                  <div className="absolute left-[68%] top-[65%]">
-                    <img
-                      src={cardboardCatImage}
-                      alt="Cardboard Cat"
-                      className="w-[175px] h-[140px]"
-                    />
-                    <div className="absolute inset-0 flex items-center justify-center">
-                      <span className="font-pixelify text-[16px] text-black text-center max-w-[120px]">
-                        Some more songs you'd like by them!
-                      </span>
-                    </div>
-                  </div>
-
-                  {/* Navigation buttons */}
-                  <div className="absolute bottom-20 w-full flex justify-between px-16">
+                  {/* Navigation buttons in corners of content container */}
+                  <div className="absolute bottom-0 left-0 right-0 flex items-center">
                     {currentArtistIndex > 0 && (
                       <img 
                         src={pageFlipLeft}
                         alt="Previous Artist"
-                        className="w-[100px] cursor-pointer hover:scale-105 transition-transform duration-200"
+                        className="absolute -bottom-2 -left-2 w-[100px] cursor-pointer hover:scale-105 transition-transform duration-200"
                         onClick={handlePrevArtist}
                       />
                     )}
@@ -483,14 +348,14 @@ const TopArtistsUI: React.FC<TopArtistsUIProps> = ({ topArtists, timeRange, setT
                       <img 
                         src={pageFlipRight}
                         alt="Next Artist"
-                        className="w-[100px] cursor-pointer hover:scale-105 transition-transform duration-200"
+                        className="absolute -bottom-4 right-2 w-[100px] cursor-pointer hover:scale-105 transition-transform duration-200"
                         onClick={handleNextArtist}
                       />
                     )}
                   </div>
 
                   {/* Time range buttons */}
-                  <div className="absolute bottom-8 left-[20%] flex gap-8">
+                  <div className="absolute bottom-0 left-1/2 -translate-x-1/2 flex justify-center gap-8">
                     <button 
                       className={`font-pixelify text-xl hover:text-green-700 transition-colors duration-200 ${timeRange === 'short_term' ? 'text-green-700' : 'text-black'}`}
                       onClick={() => setTimeRange('short_term')}
@@ -509,6 +374,149 @@ const TopArtistsUI: React.FC<TopArtistsUIProps> = ({ topArtists, timeRange, setT
                     >
                       All Time
                     </button>
+                  </div>
+                  <div className="absolute ml-12 top-16">
+                    <img
+                      src={currentArtist.albumImageUrl}
+                      alt={currentArtist.name}
+                      className="w-[175px] aspect-square"
+                    />
+                  </div>
+
+                  {/* Random album image */}
+                  <div className="absolute left-64 top-[45%]">
+                    <img
+                      src={currentArtist.randomAlbumImage}
+                      alt="Random Album"
+                      className="w-[175px] aspect-square"
+                    />
+                  </div>
+
+                  {/* Artist info section */}
+                  <div className="absolute left-72 top-16">
+                    <span className="font-pixelify text-[48px] text-black">#{currentArtistIndex + 1}</span>
+                    <span className="font-pixelify text-[20px] text-black ml-4 inline-block translate-y-4">
+                      {currentArtist.name}
+                    </span>
+                    {/* genre */}
+                    <div className="font-pixelify text-[20px] text-black mt-[15%] -ml-8 max-w-[186px]">
+                      {displayText}
+                      {hasMore && (
+                        <button
+                          onClick={() => setIsExpanded(!isExpanded)}
+                          className="text-green-700 hover:text-green-900 ml-2 underline cursor-pointer"
+                        >
+                          {isExpanded ? 'Show Less' : 'Show More'}
+                        </button>
+                      )}
+                    </div>
+                  </div>
+
+                  {/* followers */}
+                  <div className="absolute left-12 top-72 w-44 font-pixelify text-[20px] text-black">
+                    {`They have ${currentArtist.followers.toLocaleString()} followers${currentArtist.isFollowed ? " and you're one of them!" : '!'}`}
+                  </div>
+
+                  {/* Decorative elements */}
+                  <img
+                    src={starImage}
+                    alt="Star 1"
+                    className="absolute w-[75px] aspect-square left-96 top-60"
+                  />
+                  <img
+                    src={starImage}
+                    alt="Star 2"
+                    className="absolute w-[75px] aspect-square left-40 top-96"
+                  />
+                  <img
+                    src={buttonImage}
+                    alt="Button"
+                    className="absolute w-[50px] aspect-square left-80 bottom-36"
+                  />
+                  <img
+                    src={heartImage}
+                    alt="Heart"
+                    className="absolute w-[50px] aspect-square right-72 bottom-96"
+                  />
+
+                  {/* Jellyfish section with top tracks */}
+                  <div className="absolute right-72 top-24">
+                    <div className="relative">
+                      <img
+                        src={jellyfishImage}
+                        alt="Jellyfish"
+                        className="w-full h-full max-w-[200px]"
+                      />
+                      <div className="absolute inset-0 flex top-12 justify-center">
+                        <span className="font-pixelify text-[18px] text-black text-center w-24">
+                          Their top 5 songs are
+                        </span>
+                      </div>
+                    </div>
+
+                    <div className="absolute ml-56 top-12 flex flex-col gap-2 w-[180px]">
+                      {currentArtist.topTracks?.slice(0, 5).map((track, index) => (
+                        <div key={index} className="flex items-center gap-2">
+                          <span className="text-[20px] font-bold">{index + 1}.</span>
+                          <a 
+                            href={`https://open.spotify.com/track/${track.uri.split(':')[2]}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="text-[20px] font-pixelify hover:text-green-700 transition-colors duration-200 truncate"
+                          >
+                            {track.name}
+                          </a>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+
+                  {/* Ribbon and recommendations section */}
+                  <div className="absolute right-72 bottom-56">
+                    <div className="relative">
+                      <img
+                        src={ribbonImage}
+                        alt="Ribbon"
+                        className="w-[200px] h-[140px]"
+                      />
+
+                      {/* recommendations */}
+                      <div className="absolute inset-0 flex items-center justify-center top-44">
+                        <div className="flex flex-col gap-3 w-[144px]">
+                          {loading ? (
+                            <div className="font-pixelify text-[20px] text-black">Loading recommendations...</div>
+                          ) : recommendations.length > 0 ? (
+                            recommendations.map((track, index) => (
+                              <a 
+                                key={track.uri}
+                                href={`https://open.spotify.com/track/${track.uri.split(':')[2]}`}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-[20px] font-pixelify hover:text-green-700 transition-colors duration-200 truncate"
+                              >
+                                {index + 1}. {track.name}
+                              </a>
+                            ))
+                          ) : (
+                            <div>No recommendations found</div>
+                          )}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  {/* Cardboard Cat section */}
+                  <div className="absolute right-20 bottom-20">
+                    <img
+                      src={cardboardCatImage}
+                      alt="Cardboard Cat"
+                      className="w-[175px] h-[140px]"
+                    />
+                    <div className="absolute inset-0 flex items-center justify-center">
+                      <span className="font-pixelify text-[16px] text-black text-center max-w-[120px]">
+                        Some more songs you'd like by them!
+                      </span>
+                    </div>
                   </div>
                 </>
               )}
