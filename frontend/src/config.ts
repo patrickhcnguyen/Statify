ce26cdda421e0363273f3e78b8682b05c9b70d3f
patrickhcnguyen <patricknguyen714@gmail.com>
2025-03-06 01:36:20 -0800

    updated redirect url for prod

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -4,9 +4,9 @@ const isLocalBackend = process.env.REACT_APP_USE_LOCAL_BACKEND === 'true';
 
 export const API_BASE_URL = isDevelopment && isLocalBackend
   ? 'http://localhost:8888'
-  : 'https://statify-backend.vercel.app';
+  : 'http://18.118.20.116:8888';
 
 export const REDIRECT_URIS = [
   'http://localhost:8888/callback',
-  'https://statify-backend.vercel.app/callback'
+  'http://18.118.20.116:8888/callback'
 ]; 
